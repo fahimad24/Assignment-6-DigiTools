634a6efe8cdaa4adf13732c2160bf6d30d7e210f
fahimad24 <mdtaraquerahmanfahim@gmail.com>
2026-03-30 23:57:33 +0600

    create react project with vite and tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-amber-300">DigiTools</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
